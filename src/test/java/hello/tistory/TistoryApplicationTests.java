b4e5023b10654a62287c12f998a0f54dfbdb1e24
sungjun1116 <opr99@naver.com>
2022-09-01 23:07:43 +0900

    Initial Commit

---
+++ src/test/java/hello/tistory/TistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.tistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
