e75f0d14978e00999ee5e0a2406d7866c8d23486
sungjun1116 <opr99@naver.com>
2022-09-11 21:00:23 +0900

    feat: 로그인 처리(세션)

--- src/main/java/hello/login/HomeController.java
+++ src/main/java/hello/login/HomeController.java
@@ -1,34 +1,25 @@
 package hello.login;
 
 import hello.login.domain.member.Member;
-import hello.login.domain.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import hello.login.web.session.SessionConst;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
-@RequiredArgsConstructor
 public class HomeController {
 
-    private final MemberRepository memberRepository;
-
     @GetMapping("/")
-    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
 
-        if (memberId == null) {
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
             return "home";
         }
 
-        // 로그인
-        Optional<Member> loginMember = memberRepository.findById(memberId);
-        if (loginMember.isPresent()) {
-            model.addAttribute("member", loginMember.get());
-            return "loginHome";
-        }
-        return "home";
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.session.SessionConst;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -28,17 +29,20 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
 
         Optional<Member> loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        // 로그인 성공 처리
         if (loginMember.isPresent()) {
-            // 로그인 성공 처리
-            // 쿠키에 시간 정보를 주지 않으면 세션 쿠기(브라우저 종료시 모두 종료)
-            Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.get().getId()));
-            response.addCookie(idCookie);
+
+            // 세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+            HttpSession session = request.getSession();
+
+            // 세션에 로그인 회원 정보 보관
+            session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember.get());
             return "redirect:/";
         }
         bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
@@ -46,14 +50,12 @@ public class LoginController {
     }
 
     @PostMapping("/logout")
-    public String logout(HttpServletResponse response) {
-        expireCookie(response, "memberId");
+    public String logout(HttpServletRequest request) {
+        // 세션을 삭제
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
         return "redirect:/";
-    }
-
-    private void expireCookie(HttpServletResponse response, String cookieName) {
-        Cookie cookie = new Cookie(cookieName, null);
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
     }
 }
