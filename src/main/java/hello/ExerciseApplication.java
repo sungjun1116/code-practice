d08c74e81488836a13bdfacd3a43cb4456f537d8
sungjun1116 <opr99@naver.com>
2023-02-04 21:24:48 +0900

    chore: 자바 코드 구성정보 사용

--- src/main/java/hello/ExerciseApplication.java
+++ src/main/java/hello/ExerciseApplication.java
@@ -1,17 +1,27 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@SpringBootApplication
+@Configuration
 public class ExerciseApplication {
+    @Bean
+    public HelloController helloController(final HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
@@ -24,9 +34,8 @@ public class ExerciseApplication {
                 webServer.start();
             }
         };
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh();
+         applicationContext.register(ExerciseApplication.class);
+         applicationContext.refresh();
     }
 
 }
