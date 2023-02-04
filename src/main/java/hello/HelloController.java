dc7369815ee5528276c86eb641782c803dcd2f03
sungjun1116 <opr99@naver.com>
2023-02-04 19:30:40 +0900

    feat: 의존 오브젝트 DI 적용

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,9 +3,13 @@ package hello;
 import java.util.Objects;
 
 public class HelloController {
-    public String hello(String name) {
-        SimpleHelloService helloService = new SimpleHelloService();
+    private final HelloService helloService;
 
+    public HelloController(final HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 
---
+++ src/main/java/hello/HelloService.java
@@ -0,0 +1,5 @@
+package hello;
+
+public interface HelloService {
+    String sayHello(String name);
+}
