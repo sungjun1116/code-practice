ad4ae92a57e27a343524876fd2fbfbdced4c8d1a
sungjun1116 <opr99@naver.com>
2023-02-04 22:54:21 +0900

    chore: SpringApplication

--- src/main/java/hello/ExerciseApplication.java
+++ src/main/java/hello/ExerciseApplication.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +23,7 @@ public class ExerciseApplication {
     }
 
     public static void main(String[] args) {
-        MySpringApplication.run(ExerciseApplication.class, args);
+        SpringApplication.run(ExerciseApplication.class, args);
     }
 
 }
