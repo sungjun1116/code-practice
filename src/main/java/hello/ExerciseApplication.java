f989a438281d34e53135d1f56116d162eab6a51f
sungjun1116 <opr99@naver.com>
2023-02-04 22:52:00 +0900

    chore: MySpringApplication.java

--- src/main/java/hello/ExerciseApplication.java
+++ src/main/java/hello/ExerciseApplication.java
@@ -1,13 +1,12 @@
 package hello;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+
 
 @Configuration
 @ComponentScan
@@ -23,25 +22,7 @@ public class ExerciseApplication {
     }
 
     public static void main(String[] args) {
-         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-                // 스프링 컨테이너가 자동으로 주입해줌 ctrl + H 눌러서 구현하고 있는 인터페이스 확인해보자(ApplicationContextAware)
-                // dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-         applicationContext.register(ExerciseApplication.class);
-         applicationContext.refresh();
+        MySpringApplication.run(ExerciseApplication.class, args);
     }
 
 }
---
+++ src/main/java/hello/MySpringApplication.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MySpringApplication {
+    public static void run(Class<?> applicationClass, String... args) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                // 스프링 컨테이너가 자동으로 주입해줌 ctrl + H 눌러서 구현하고 있는 인터페이스 확인해보자(ApplicationContextAware)
+                // dispatcherServlet.setApplicationContext(this);
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                            .addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+        applicationContext.register(applicationClass);
+        applicationContext.refresh();
+    }
+}
