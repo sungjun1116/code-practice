8377ce0edcfc4e55b375579e445c1b900922c9bd
sungjun1116 <opr99@naver.com>
2023-02-04 16:54:02 +0900

    feat: 서블릿 컨테이너 띄우기

--- src/main/java/hello/ExerciseApplication.java
+++ src/main/java/hello/ExerciseApplication.java
@@ -1,13 +1,17 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 @SpringBootApplication
 public class ExerciseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExerciseApplication.class, args);
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 
 }
