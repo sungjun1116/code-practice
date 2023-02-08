d60bc54d215301b90d5551de59b1a879765cc1b7
sungjun1116 <opr99@naver.com>
2023-02-08 23:38:27 +0900

    test: TestRestTemplate 을 이용한 API 테스트

---
+++ src/test/java/hello/HelloApiTest.java
@@ -0,0 +1,29 @@
+package hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status code 200
+        assertThat(res.getStatusCode()).isSameAs(HttpStatus.OK);
+
+        // header(content-type) text/plain
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+
+        // body Hello Spring
+        assertThat(res.getBody()).isEqualTo("Hello Spring");
+
+    }
+}
