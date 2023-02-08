e8585de3d9fd92d8e2631a3b77ea497299a63979
sungjun1116 <opr99@naver.com>
2023-02-09 00:25:00 +0900

    test: DI와 단위 테스트

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,8 +3,6 @@ package hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
 @RestController
 public class HelloController {
     private final HelloService helloService;
@@ -15,7 +13,10 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return helloService.sayHello(Objects.requireNonNull(name));
+        if (name == null || name.trim().length() == 0) {
+            throw  new IllegalArgumentException();
+        }
+        return helloService.sayHello(name);
     }
 
 }
