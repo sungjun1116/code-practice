5c1edb4a94157e64a6bed4c5375f6682d264aa7a
sungjun1116 <opr99@naver.com>
2023-02-11 16:34:47 +0900

    @Value와 PropertySourcesPlaceholderConfigure

---
+++ src/main/java/hello/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,13 @@
+package hello.config.autoconfig;
+
+import hello.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
--- src/main/java/hello/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/hello/config/autoconfig/TomcatWebServerConfig.java
@@ -2,20 +2,23 @@ package hello.config.autoconfig;
 
 import hello.config.ConditionalMyOnClass;
 import hello.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        serverFactory.setContextPath(env.getProperty("contextPath"));
+        serverFactory.setContextPath(this.contextPath);
         return serverFactory;
     }
 }
